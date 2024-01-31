9ffea6e03afb544617b8710fd1e55ec4f119fe50
Gillian Perard <GillianPerard@users.noreply.github.com>
2024-01-31 16:44:23 +0100

    chore: fix all lint and format errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  imports: [RouterOutlet],
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'quizz';
